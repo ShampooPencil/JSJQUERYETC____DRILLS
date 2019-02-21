57798452dce5ef398b87824cb25b9ca6020c8aca
DrewDrizzle <aramouni8@gmail.com>
2019-02-20 23:09:32 -0500

    added lazyBOB

--- lazyBOB.js
+++ lazyBOB.js
@@ -1,13 +1,21 @@
-function MaybeTalktoBob(){
+function MaybeTalkToBob(){
     //start of with a if/else but come and modify the programm so i can practice
     //and learn case conditions master cases and much more
-    tlkToBob = "Yes";
-    let shouldiTalkToBob = prompt("Do you want to talk to Bob? Yes/No");
-    if (talkToBob === shouldiTalkToBob){
-        AskBob();
-    }
-
+    //console.log("testing");
+    var talkToBob = "Y";
+    var notTalkToBob = "N";
+    var shouldiTalkToBob = prompt("Do you want to talk to Bob? Y/N");
+    if (talkToBob === shouldiTalkToBob.toUpperCase()){
+        return AskBob();
+    } else if (notTalkToBob === shouldiTalkToBob.toUpperCase()) {
+       return MaybeTalkToBob();
+    };
 };
 function AskBob(){
-
-}
+    console.log("TESTING 2nd");
+    //document.getElementById("BOB").innerHTML = "TESTING";
+    var firstSentence = document.getElementById("BOB").innerHTML;
+    firstSentence = "Whats Up Bob";
+    return firstSentence;
+};
+MaybeTalkToBob();
